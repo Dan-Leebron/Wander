abeed42bc5a39b9c8905f5b815073bb8ad1afbb4
Dan-Leebron <Dan-Leebron@users.noreply.github.com>
2019-09-10 04:38:48 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -16,19 +16,24 @@ int main(void)
         {
             printf(" ");
         }
+        //printing the spaces for the first half of the pyramid
         for (int k = 0; k < i + 1; k++)
         {
             printf("#");
         }
+        //printing the blocks for the first half of the pyramid
         printf("  ");
+        //printing the middle two spaces
         for (int k = 0; k < i + 1; k++)
         {
             printf("#");
         }
+        //printing the blocks of the second half of the pyramid, same number as the first half
         for (int j = 0; j < height - i - 1; j++)
         {
             printf(" ");
         }
+        //printing the spaces for the second half of the pyramid, same as the first half
         printf("\n");
     }
 }
