d39e45857bcfb80a6f787e4100dff959f4a27f04
Dan-Leebron <Dan-Leebron@users.noreply.github.com>
2019-09-17 02:56:55 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -15,14 +15,17 @@ int main(void)
     //seeing how many letters are in the string
     for (int i = 0; i < n; i++)
     {
+        //counts alphabetical characters as letters
         if (isalpha(text[i]))
         {
             let++;
         }
-        if (isspace(text[i+1]))
+        //counts words in the text
+        if (isspace(text[i + 1]))
         {
             words++;
         }
+        //counts sentences in the text
         if (text[i] == '.' || text[i] == '?' || text[i] == '!')
         {
             sent++;
@@ -31,9 +34,11 @@ int main(void)
     printf("%i letter(s)\n", let);
     printf("%i words(s)\n", words);
     printf("%i sentence(s)\n", sent);
+    //calculations for the index
     float L = 100 * (float) let / words;
     float S = 100 * (float) sent / words;
-    int index  = round(0.0588 * L - 0.296 * S -15.8);
+    int index  = round(0.0588 * L - 0.296 * S - 15.8);
+    //including cases where the grade is too high or low
     if (index > 16)
     {
         printf("Grade 16+\n");
