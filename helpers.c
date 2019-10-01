a687f73766c9a18aabe08fcb6c15c05270a003b0
Dan-Leebron <Dan-Leebron@users.noreply.github.com>
2019-10-01 02:47:58 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -54,6 +54,23 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0, k = round(width / 2); j < k; j++)
+        {
+            int tmpred = image[i][j].rgbtRed;
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+            image[i][width - j - 1].rgbtRed = tmpred;
+
+            int tmpgreen = image[i][j].rgbtGreen;
+            image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
+            image[i][width - j - 1].rgbtGreen = tmpgreen;
+
+            int tmpblue = image[i][j].rgbtBlue;
+            image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
+            image[i][width - j - 1].rgbtBlue = tmpblue;
+        }
+    }
     return;
 }
 
