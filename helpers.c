4747fd62c9504b440b88c93a91695f55f3e114e2
Dan-Leebron <Dan-Leebron@users.noreply.github.com>
2019-10-01 02:17:46 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -20,6 +20,34 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+
+         float newred = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
+         float newgreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+         float newblue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+
+         if (round(newred) > 255)
+         {
+             newred = 255;
+         }
+         if (round(newgreen) > 255)
+         {
+             newgreen = 255;
+         }
+         if (round(newblue) > 255)
+         {
+             newblue = 255;
+         }
+
+         image[i][j].rgbtRed = newred;
+         image[i][j].rgbtGreen = newgreen;
+         image[i][j].rgbtBlue = newblue;
+
+        }
+    }
     return;
 }
 
