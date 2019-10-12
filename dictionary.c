80de93c8471c082ac7f13921b5bafe2ee636e8cc
Dan-Leebron <Dan-Leebron@users.noreply.github.com>
2019-10-12 19:10:29 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -30,6 +30,7 @@ node *table[N];
 bool check(const char *word)
 {
     int hashed = hash(word);
+    //running hash function on new word, if the word is in the linked list corresponding to that hash number, return true
     for (node *cursor = table[hashed]; cursor != NULL; cursor = cursor -> next)
     {
         if ((strcasecmp(word, cursor -> word) == 0))
@@ -45,21 +46,24 @@ unsigned int hash(const char *word)
 {
     //Using the hash function sdbm from https://www.programmingalgorithms.com/algorithm/sdbm-hash/c/
     unsigned int hash = 0;
-	unsigned int i = 0;
-	int length = strlen(word);
-	int lowerword[length];
+    unsigned int i = 0;
+    int length = strlen(word);
+    int lowerword[length];
 
-	for (int j = 0; j < length; j++)
-	{
-	    lowerword[j] = tolower(word[j]);
-	}
+    //converting the word to lowercase so the hash function does not return different values for different capitalizations
+    for (int j = 0; j < length; j++)
+    {
+        lowerword[j] = tolower(word[j]);
+    }
 
-	for (i = 0; i < length; word++, i++)
-	{
-		hash = (*lowerword) + (hash << 6) + (hash << 16) - hash;
-	}
+    //where the magic happens! Big shoutout to whoever made the sdbm hash function
+    for (i = 0; i < length; word++, i++)
+    {
+        hash = (*lowerword) + (hash << 6) + (hash << 16) - hash;
+    }
 
-	return (hash % N);
+    //making sure the number returned is within the buckets
+    return (hash % N);
 }
 
 // Loads dictionary into memory, returning true if successful else false
@@ -78,18 +82,21 @@ bool load(const char *dictionary)
     {
 
         fscanf(file, "%s", text);
+        //making sure we close the file and whatnot if we reach the end of the file. loadstatus comes back in the size function
         if (feof(file))
         {
             fclose(file);
             loadstatus = true;
             return true;
         }
+        //wcount gives the amount of words for the size function
         wcount++;
         node *n = malloc(sizeof(node));
         if (n == NULL)
         {
             return false;
         }
+        //adding new nodes to the head of whatever bucket in the hash function they fall into
         strcpy(n -> word, text);
         n -> next = NULL;
         int i = hash(text);
@@ -103,6 +110,7 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
+    //pretty simple, return the wordcount if it is loaded
     if (loadstatus)
     {
         return wcount;
@@ -116,6 +124,7 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
+    //also pretty simple, just running through the linked list for each bucket in the hash file and freeing the list starting from the head
     for (int i = 0; i < N; i++)
     {
         node *ptr = table[i];
