3fc8c6ea31cbb09adbea6d2786b1f7e26d40692c
Dan-Leebron <Dan-Leebron@users.noreply.github.com>
2019-10-27 19:30:14 +0000

    automated commit by submit50

--- cash.py
+++ cash.py
@@ -4,10 +4,11 @@ while True:
     n = get_float("Change owed: ")
     if n > 0:
         break
-
+# converting the money amount into coins
 n *= 100
 coins = 0
 
+# flowing through everything to count the total number of coins
 while n >= 25:
     n -= 25
     coins += 1
@@ -17,4 +18,5 @@ while n >= 10:
 while n >= 5:
     n -= 5
     coins += 1
+# printing the final number of coins owed
 print(f"{int(coins + n)}")
