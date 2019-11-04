ff8efc9702971bc23bd853da7a8e50b2f1aa509f
Dan-Leebron <Dan-Leebron@users.noreply.github.com>
2019-11-04 02:02:29 +0000

    automated commit by submit50

--- import.py
+++ import.py
@@ -7,7 +7,7 @@ import cs50
 if len(sys.argv) != 2:
     print("Usage: python import.py characters.csv")
     exit()
-
+# associating the database
 db = cs50.SQL("sqlite:///students.db")
 
 with open(sys.argv[1], "r") as titles:
@@ -16,7 +16,7 @@ with open(sys.argv[1], "r") as titles:
 
     for row in reader:
         nameparts = row["name"].split()
-
+# if there is a middle name, including it, or if not then making it null
         if len(nameparts) == 3:
             first = nameparts[0]
             mid = nameparts[1]
@@ -25,6 +25,7 @@ with open(sys.argv[1], "r") as titles:
             first = nameparts[0]
             mid = None
             last = nameparts[1]
-
-        db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", first, mid, last, row["house"], row["birth"])
+# putting everything into the database
+        db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
+                   first, mid, last, row["house"], row["birth"])
 
